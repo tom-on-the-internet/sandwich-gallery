dbade1c2a7397728ee0cc00afcdda264ac41c499
Thomas Steven <thomaspatricksteven@gmail.com>
2021-03-04 21:13:27 -0500

    mmm

--- sandwich.js
+++ sandwich.js
@@ -7,10 +7,28 @@ const sandwichThought = [
   "Get in my belly you yummy sandwich",
   "Uh, yes please!",
   "Christ! Now that's a sandwich",
+  "Sandwich time!!",
+  "Look at that thing! Wowza!",
 ];
 
-console.log("hit");
-const index = Math.floor(Math.random() * sandwichThought.length);
-const saying = sandwichThought[index];
-const utterance = new SpeechSynthesisUtterance(saying);
-synth.speak(utterance);
+fetch(
+  "https://pixabay.com/api/?key=1104590-c66d2c68dbb582ba6e0d74373&q=sandwich&image_type=photo&per_page=200&category=food"
+).then((res) => {
+  res.json().then((res) => {
+    const hits = res.hits;
+    const hitsIndex = Math.floor(Math.random() * hits.length);
+    const hit = hits[hitsIndex];
+    const imageURL = hit.largeImageURL;
+
+    const $sandwich = document.getElementById("sandwich");
+    $sandwich.addEventListener("load", () => {
+      console.log("loaded");
+      const thoughtIndex = Math.floor(Math.random() * sandwichThought.length);
+      const saying = sandwichThought[thoughtIndex];
+      const utterance = new SpeechSynthesisUtterance(saying);
+      utterance.rate = 0.7;
+      synth.speak(utterance);
+    });
+    $sandwich.src = imageURL;
+  });
+});
